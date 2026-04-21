cf3680b48022c2d711b63ec3b5ee072cdc41b8d0
darth.revan.030 <smartsamarth03@gmail.com>
2026-04-21 23:55:32 +0800

    feat: implement input reading and validation for Redis commands

--- app/main.go
+++ app/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"bufio"
+	"strconv"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
@@ -29,22 +32,45 @@ func handleClient(conn net.Conn) {
 func main() {
 
 	// listens on port 6379 and exits with an error if the server fails to start
-	listener, err := net.Listen("tcp", "localhost:6379")
-	if err != nil {
-		fmt.Println("error starting server: ", err.Error())
+	// listener, err := net.Listen("tcp", "localhost:6379")
+	// if err != nil {
+	// 	fmt.Println("error starting server: ", err.Error())
+	// 	os.Exit(1)
+	// }
+
+	// defer listener.Close()
+
+	// for {
+	// 	conn, err := listener.Accept()
+	// 	if err != nil {
+	// 		fmt.Println("Error accepting connection", err.Error())
+	// 		continue
+	// 	}
+		
+	// 	go handleClient(conn)
+	// }
+
+	input := "$5\n\rREDIS\r\n"
+
+	reader := bufio.NewReader(strings.NewReader(string(input)))
+	
+	b, _ := reader.ReadByte()
+
+	if b != '$' {
+		fmt.Println("invalid type, expecting bulk strings only")
 		os.Exit(1)
 	}
 
-	defer listener.Close()
+	size, _ := reader.ReadByte()
 
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection", err.Error())
-			continue
-		}
-		
-		go handleClient(conn)
-	}
+	strSize, _ := strconv.ParseInt(string(size), 10, 64)
+
+	// consume \r and \n
+	reader.ReadByte()
+	reader.ReadByte()
+
+	outputBuff := make([]byte, strSize)
+
+	reader.Read(outputBuff)
 
 }
